c669e55a3566003ef4fd7ac8f9d750f803f83701
Daniel Schnelle <daniel_schnelle@web.de>
2023-06-29 17:47:51 +0200

    Add a wildcard route

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,11 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController {
     #[Route('/')]
     public function homepage() {
         die("Welcome to The Network");
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null) {
+        $topic = \symfony\component\string\u(str_replace('-', ' ', $slug))->title(true);
+        return new Response('Talk to your family? Meet new friends? See what\'s going on'.($slug ? ' about '.$topic :'').'!');
+
     }
 }

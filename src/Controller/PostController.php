0011afa32fc0125c6ff6857916ffb44b62dcabbd
Daniel Schnelle <daniel_schnelle@web.de>
2023-07-04 21:18:01 +0200

    Add cs fixer

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,24 +2,26 @@
 
 namespace App\Controller;
 
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController extends AbstractController {
+class PostController extends AbstractController
+{
     #[Route('/')]
     public function homepage(): Response
     {
         return $this->render('post/homepage.html.twig', [
-            'date' => new DateTime(),
+            'date' => new \DateTime(),
             'text' => 'Welcome to The Network everybody 😎',
         ]);
     }
-    #[Route('/browse/{slug}')]
-    public function browse(string $slug = null) {
-        $topic = \symfony\component\string\u(str_replace('-', ' ', $slug))->title(true);
-        return new Response('Talk to your family? Meet new friends? See what\'s going on'.($slug ? ' about '.$topic :'').'!');
 
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null)
+    {
+        $topic = \symfony\component\string\u(str_replace('-', ' ', $slug))->title(true);
+
+        return new Response('Talk to your family? Meet new friends? See what\'s going on'.($slug ? ' about '.$topic : '').'!');
     }
 }

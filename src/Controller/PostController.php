3033afae11428bbaf8ecc55a0283373094a6090f
Daniel Schnelle <daniel_schnelle@web.de>
2023-07-11 14:20:45 +0200

    Display post text from the database

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    public function __construct(private PostRepository $postRepository)
+    {     
+    }
+
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
+        $posts = $this->postRepository->findAll();
+
+        $text = $posts[0] ? $posts[0]->getContent() : 'No posts available!';
+
         return $this->render('post/homepage.html.twig', [
             'date' => new \DateTime(),
-            'text' => 'Welcome to The Network everybody 😎',
+            'text' => $text,
         ]);
     }
 
